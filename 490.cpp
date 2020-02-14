beceacc577e0475977e250e743f061554ff59166
bn <bn@127.com>
2020-02-14 22:49:11 +0800

    init

---
+++ 490.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        int x;
+        for(int i = 0;i<10;i++) {
+            x += i;
+        }
+        return x;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> maze;
+    vector<int> start;
+    vector<int> destination;
+    Solution s;
+    return 0;
+}
